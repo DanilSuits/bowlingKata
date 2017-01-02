d711c30a75993965cdd3c89d25303befb6791864
Danil <danil@vast.com>
2017-01-02 15:04:34 -0600

    GREEN: Introducing a test specification

--- src/test/java/com/vocumsineratio/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/vocumsineratio/kata/bowling/BowlingGameTest.java
@@ -14,20 +14,33 @@ public class BowlingGameTest {
     public void testGutterGame() {
         final int ballsPerGame = 20;
         final int pinsKnockedDown = 0;
-        final int expectedScore = Game.INITIAL_SCORE;
         int[] ballsThrown = new int[ballsPerGame];
-
         Arrays.fill(ballsThrown, pinsKnockedDown);
 
+        final int expectedScore = Game.INITIAL_SCORE;
+
+        // Everything that happens above this break is just specification design
+
+        Specification gutterGameSpec = new Specification(ballsThrown, expectedScore);
+
+
         Game g = new Game();
-        for (int pins : ballsThrown) {
+        for (int pins : gutterGameSpec.ballsThrown) {
             g.roll(pins);
         }
 
-        Assert.assertEquals(g.score(), expectedScore);
+        Assert.assertEquals(g.score(), gutterGameSpec.expectedScore);
     }
 }
+class Specification {
+    final int [] ballsThrown;
+    final int expectedScore;
 
+    Specification(int[] ballsThrown, int expectedScore) {
+        this.ballsThrown = ballsThrown;
+        this.expectedScore = expectedScore;
+    }
+}
 class Game {
     static final int INITIAL_SCORE = 0;
 
