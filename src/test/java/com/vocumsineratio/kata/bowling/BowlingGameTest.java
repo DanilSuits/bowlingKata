3ffe12ce8a6773cfd9b6284bb4d96a14827c47e2
Danil <danil@vast.com>
2017-01-02 12:54:07 -0600

    GREEN: clarifying the different meanings of zero

--- src/test/java/com/vocumsineratio/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/vocumsineratio/kata/bowling/BowlingGameTest.java
@@ -14,22 +14,26 @@ public class BowlingGameTest {
         final int ballsPerGame = 20;
         int [] ballsThrown = new int[ballsPerGame];
         for (int i : ballsThrown) {
-            final int scoreForGutterBall = 0;
+            final int pinsKnockedDown = 0;
+
+            final int scoreForGutterBall = pinsKnockedDown * Game.SCORE_PER_PIN;
             g.roll(scoreForGutterBall);
         }
 
-        final int expectedScore = 0;
+        final int expectedScore = Game.INITIAL_SCORE;
         Assert.assertEquals(g.score(), expectedScore);
     }
 }
 class Game {
+    static final int INITIAL_SCORE = 0;
+    static final int SCORE_PER_PIN = 1;
 
     public void roll(int i) {
         //TODO: To change body of created methods use File | Settings | File Templates.
     }
 
     public int score() {
-        final int finalScore = 0;
+        final int finalScore = INITIAL_SCORE;
         return finalScore;
     }
 }
